dad22f0b51761d33db6611c13f69d535837d4210
Navas-uni <ang1007@alu.ubu.es>
2025-03-23 13:24:11 +0100

    Update ChatServer.java

--- src/main/java/es/ubu/lsi/server/ChatServer.java
+++ src/main/java/es/ubu/lsi/server/ChatServer.java
@@ -1,5 +1,63 @@
 package es.ubu.lsi.server;
 
-public class ChatServer {
+import es.ubu.lsi.common.ChatMessage;
+
+/**
+
+* Interfaz que define las operaciones básicas del servidor de chat.
+
+* Incluye métodos para arranque, multidifusión de mensajes,
+
+* eliminación de clientes y apagado del servidor.
+
+*
+
+* @author Alejandro Navas García
+
+* @author Nicolás Muñoz Miguel
+
+*/
+
+public interface ChatServer {
+
+/**
+
+* Inicia el servidor, configurando los recursos necesarios.
+
+*/
+
+void startup();
+
+/**
+
+* Cierra todas las conexiones activas y apaga el servidor.
+
+*/
+
+void shutdown();
+
+/**
+
+* Envía un mensaje a todos los clientes conectados.
+
+*
+
+* @param message Mensaje que será enviado a los clientes.
+
+*/
+
+void broadcast(ChatMessage message);
+
+/**
+
+* Elimina un cliente de la lista de clientes activos.
+
+*
+
+* @param client Cliente que será eliminado.
+
+*/
+
+void remove(int id);
 
 }
